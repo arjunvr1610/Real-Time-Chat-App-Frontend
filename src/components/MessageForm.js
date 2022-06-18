1fa53833760b6f47893d7f6156ea8505eb94d8d4
Arjun V R <arjunvr1610@gmail.com>
2022-06-19 00:52:53 +0530

    second commit

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { Col, Form, Row, Button } from 'react-bootstrap'
 import { useSelector } from 'react-redux';
 import { AppContext } from '../context/appContext';
@@ -7,7 +7,12 @@ import './MessageForm.css'
 const MessageForm = () => {
     const [message, setMessage] = useState('');
     const user = useSelector(state => state.user);
-    const { socket, messages, setMessages, currentRoom } = useContext(AppContext);
+    const { socket, messages, setMessages, currentRoom, privateMemberMsgs } = useContext(AppContext);
+    const messageEndRef = useRef(null);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages])
 
     const getFormattedDate = () => {
         const date = new Date()
@@ -19,46 +24,64 @@ const MessageForm = () => {
         return month + '/' + day + '/' + year
     }
 
+    const scrollToBottom = () => {
+        messageEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+
     const todayDate = getFormattedDate()
 
-    socket.off('room-messages').on('room-messages', roomMessages => {setMessages(roomMessages)})
+    socket.off('room-messages').on('room-messages', roomMessages => { setMessages(roomMessages) })
+
+    socket.off('message-room').on('message-room', roomMessages => {
+        setMessages(roomMessages)
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if(!message) return;
+        if (!message) return;
         const today = new Date()
         const minutes = today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes()
         const time = today.getHours() + ':' + minutes
         const roomId = currentRoom
         socket.emit('message-room', roomId, message, user, time, todayDate)
-        setMessage('')
+        setMessage("")
+    }
+
+    const submitByEnterKey = (event) => {
+        if (event.keyCode === 13) {
+            handleSubmit()
+        }
     }
     return (
         <>
             {!user && <div className='alert alert-danger' >Please login to join chat</div>}
-            <div className="messages-output">
-                {user && messages.map(({_id: date, messagesByDate}, index) => (
+            <div className="messages-output my-2">
+                {user && messages.map(({ _id: date, messagesByDate }, index) => (
                     <div key={index}>
-                        <p className='aler alert-info text-center message-date-indicator'>{date}</p>
+                        <p className='text-center' style={{ fontWeight: 'bold' }}>{date}</p>
                         {
-                            messagesByDate.map(({content, time, from: sender}, msgIndex) => (
-                                <div key={msgIndex}>
-                                    <p className="message-content">{content}</p>
-                                    
+                            messagesByDate.map(({ content, time, from: sender }, msgIndex) => (
+                                <div className={sender._id === user._id ? 'message' : 'incomming-message'}>
+                                    <div key={msgIndex} className='message-body'>
+                                        <div className='msg-sender'>{sender.name}</div>
+                                        <div className='msg-content'>{content}</div>
+                                        <div className='msg-time'>{time}</div>
+                                    </div>
                                 </div>
                             ))
                         }
                     </div>
                 ))}
+                <div ref={messageEndRef} />
             </div>
             <Form onSubmit={handleSubmit}>
                 <Row>
-                    <Col md={11}>
-                        <Form.Group>
-                            <Form.Control type='text' disabled={!user} placeholder='Type...' onChange={e => setMessage(e.target.value)} vlaue={message}></Form.Control>
+                    <Col md={10}>
+                        <Form.Group style={{border: '1px solid lightgray', borderRadius: '10px'}}>
+                            <Form.Control id='typeField' type='text' disabled={!user} placeholder='Type...' onChange={(e) => setMessage(e.target.value)} value={message} onKeyDown={e => submitByEnterKey(e)}></Form.Control>
                         </Form.Group>
                     </Col>
-                    <Col md={1}>
+                    <Col md={2}>
                         <Button variant='primary' type='submit' disabled={!user} style={{ width: '100%', backgroundColor: 'green' }}>
                             <i className='fas fa-paper-plane'></i>
                         </Button>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useContext } from 'react'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux';
-import { AppContext } from '../context/appContext'; 
+import { AppContext } from '../context/appContext';
 import { addNotifications, resetNotifications } from '../features/userSlice';
+import './Sidebar.css'
 
 const Sidebar = () => {
   const user = useSelector(state => state.user);
@@ -11,7 +12,7 @@ const Sidebar = () => {
   const { socket, rooms, setRooms, members, setMembers, currentRoom, setCurrentRoom, privateMemberMsgs, setPrivateMemberMsgs } = useContext(AppContext);
 
   useEffect(() => {
-    if(user) {
+    if (user) {
       setCurrentRoom('general');
       getRooms();
       socket.emit('join-room', 'general');
@@ -23,28 +24,28 @@ const Sidebar = () => {
     setMembers(payload);
   })
 
-  const getRooms = async() => {
+  const getRooms = async () => {
     await fetch('http://localhost:5001/rooms').then((res) => res.json()).then((data) => setRooms(data));
   }
 
   const joinRoom = (room, isPublic = true) => {
-    if(!user) return alert('Please Login!!');
-    
+    if (!user) return alert('Please Login!!');
+
     socket.emit('join-room', room, currentRoom);
     setCurrentRoom(room);
 
-    if(isPublic) { setPrivateMemberMsgs(null) }
+    if (isPublic) { setPrivateMemberMsgs(null) }
 
     //dispatch notification
     dispatch(resetNotifications(room));
   }
 
   socket.off('notifications').on('notifications', (room) => {
-    if(currentRoom !== room) { dispatch(addNotifications(room)) }
+    if (currentRoom !== room) { dispatch(addNotifications(room)) }
   })
 
   const orderIds = (id1, id2) => {
-    if(id1 > id2) return id1 + '-' + id2;
+    if (id1 > id2) return id1 + '-' + id2;
     else return id2 + '-' + id1;
   }
 
@@ -52,32 +53,37 @@ const Sidebar = () => {
     setPrivateMemberMsgs(member)
     const roomId = orderIds(user._id, member._id)
     joinRoom(roomId, false)
-
   }
 
   return (
     <>
-    { user && (
-      <>
-        <h2 className='my-2'>Rooms</h2>
-        <ListGroup>
-          {
-            rooms.map((room, index) => (
-              <ListGroupItem key={index} active={room === currentRoom} onClick={() => joinRoom(room)} style={{cursor: 'pointer', display: 'flex', justifyContent: 'space-between'}}>{room}{currentRoom !== room && <span className='badge, rounded-pill bg-primary'>{user.newMessages[room]}</span>}</ListGroupItem>
-            ))
-          }
-        </ListGroup>
-        <h2 className='my-2'>Members Available</h2>
-        <ListGroup>
-          {
-            members.map((member) => (
-              <ListGroupItem key={member._id} active={privateMemberMsgs?._id === member?._id} style={{cursor: 'pointer'}} onClick={() => handlePrivateMemberMsgs(member)} disabled={member._id === user._id}>{member.name}</ListGroupItem>
-            ))
-          }
-        </ListGroup>
-      </>
-    )}
-    </>       
+      {user && (
+        <>
+          <h2 className='my-2'>Rooms</h2>
+          <ListGroup style={{ border: '1px solid lightgray' }} className='rooms'>
+            {
+              rooms.map((room, index) => (
+                <ListGroupItem key={index} active={room === currentRoom} onClick={() => joinRoom(room)} style={{ cursor: 'pointer', display: 'flex', justifyContent: 'space-between', fontSize: 'medium', fontWeight: 'bolder' }}>{room}{currentRoom !== room && <span className='badge rounded-pill bg-primary'>{user.newMessages[room]}</span>}</ListGroupItem>
+              ))
+            }
+          </ListGroup>
+          <h2 className='my-2'>Members Available</h2>
+          <ListGroup style={{ border: '1px solid lightgray' }} className='members'>
+            {
+              members.map((member) => (
+                <ListGroupItem key={member._id} active={privateMemberMsgs?._id === member?._id} style={{ cursor: 'pointer'}} onClick={() => handlePrivateMemberMsgs(member)} disabled={member._id === user._id}>
+                  <Row>
+                    <Col xs={2}><img src={member.picture} style={{ width: 30, height: 30, marginLeft: 10, objectFit: "cover", borderRadius: "50%" }} /></Col>
+                    <Col xs={8}><p style={{fontSize: 'medium', fontWeight: 'bolder'}}>{member.name === user.name ? member.name + ' (You)' : member.name}{' (' + member.status + ')'}</p></Col>
+                    <Col xs={2}>{currentRoom !== member._id && <span className='badge rounded-pill bg-primary'>{user.newMessages[orderIds(member._id, user._id)]}</span>}</Col>
+                  </Row>
+                </ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        </>
+      )}
+    </>
   )
 }
 
